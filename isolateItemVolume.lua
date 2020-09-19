8882a76da374b89dd4c3fc0b654d1f522b2eecfa
Ben <ben@letskedaddle.com>
2020-09-19 01:56:11 -0400

    added scripts used for generating rhythm guitar with better attack

--- isolateItemVolume.lua
+++ isolateItemVolume.lua
@@ -1,3 +1,11 @@
+-- @noindex
+
+local activeProjectIndex = 0
+
+function print(arg)
+  reaper.ShowConsoleMsg(tostring(arg) .. "\n")
+end
+
 function emptyFunctionToPreventAutomaticCreationOfUndoPoint()
 end
 
@@ -5,213 +13,238 @@ function startUndoBlock()
 	reaper.Undo_BeginBlock()
 end
 
-function endUndoBlock(actionDescription)
+function endUndoBlock()
+	local actionDescription = "isolateItemVolume"
 	reaper.Undo_OnStateChange(actionDescription)
 	reaper.Undo_EndBlock(actionDescription, -1)
 end
 
-function redrawEnvelope()
-
-	reaper.PreventUIRefresh(1)
-
-	local track = reaper.GetTrack(0,0)
-
-	trackParameter = reaper.GetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT")
-	
-	if trackParameter == 0 then
-		reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 1)
-	else
-		reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 0)
-	end
-
-	reaper.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", trackParameter)
-
-	reaper.PreventUIRefresh(-1)
+function currentBpm()
+	local timePosition = 0
+	return reaper.TimeMap2_GetDividedBpmAtTime(activeProjectIndex, timePosition)
 end
 
+function lengthOfQuarterNote()
+	return 60/currentBpm()
+end
 
-function getDeleteTimeLoopPoints(trackEnvelope, env_point_count, startTime, endTime)
-	local set_first_start = 0
-	local set_first_end = 0
-	for i = 0, env_point_count do
-		retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(trackEnvelope,i)
+function lengthOfEighthNote()
+	return lengthOfQuarterNote()/2
+end
 
-		if startTime == time and set_first_start == 0 then
-			set_first_start = 1
-			first_start_idx = i
-			firstStartValue = valueOut
-		end
-		if endTime == time and set_first_end == 0 then
-			set_first_end = 1
-			first_end_idx = i
-			firstEndValue = valueOut
-		end
-		if set_first_end == 1 and set_first_start == 1 then
-			break
-		end
-	end
+function lengthOfSixteenthNote()
+	return lengthOfEighthNote()/2
+end
 
-	local set_last_start = 0
-	local set_last_end = 0
-	for i = 0, env_point_count do
-		retval, time, valueOut, shape, tension, selectedOut = reaper.GetEnvelopePoint(trackEnvelope,env_point_count-1-i)
+function lengthOfThirtySecondNote()
+	return lengthOfSixteenthNote()/2
+end
 
-		if startTime == time and set_last_start == 0 then
-			set_last_start = 1
-			last_start_idx = env_point_count-1-i
-			lastStartValue = valueOut
-		end
-		if endTime == time and set_last_end == 0 then
-			set_last_end = 1
-			last_end_idx = env_point_count-1-i
-			lastEndValue = valueOut
-		end
-		if set_last_start == 1 and set_last_end == 1 then
-			break
+function lengthOfSixtyFourthNote()
+	return lengthOfThirtySecondNote()/2
+end
+
+function lengthOfHundredTwentyEighthNote()
+	return lengthOfSixtyFourthNote()/2
+end
+
+--
+
+function getIndicesOfSelectedTracks()
+
+	local selectedTrackIndices = {}
+
+	local wantMasterTrack = true
+	local numberOfSelectedTracks = reaper.CountSelectedTracks2(activeProjectIndex, wantMasterTrack)
+
+	for i = 0, numberOfSelectedTracks - 1 do
+
+		local selectedTrack = reaper.GetSelectedTrack2(activeProjectIndex, i, wantMasterTrack)
+		local trackNumber = reaper.GetMediaTrackInfo_Value(selectedTrack, "IP_TRACKNUMBER")
+
+		if trackNumber == -1 then
+			selectedTrackIndices[i+1] = 0.0
+		else
+			selectedTrackIndices[i+1] = trackNumber
 		end
 	end
 
-	if firstStartValue == nil then
-		retval_startTime, firstStartValue, dVdS_startTime, ddVdS_startTime, dddVdS_startTime = reaper.Envelope_Evaluate(trackEnvelope, startTime, 0, 0)
-	end
-	if lastEndValue == nil then
-		retval_endTime, lastEndValue, dVdS_endTime, ddVdS_endTime, dddVdS_endTime = reaper.Envelope_Evaluate(trackEnvelope, endTime, 0, 0)
-	end
-
-	if lastStartValue == nil then
-		lastStartValue = firstStartValue
-	end
-	if firstEndValue == nil then
-		firstEndValue = lastEndValue
-	end
-
-	reaper.DeleteEnvelopePointRange(trackEnvelope, startTime-0.000000001, endTime+0.000000001)
-
-	return firstStartValue, lastStartValue, firstEndValue, lastEndValue
-
+	return selectedTrackIndices
 end
 
-function print(arg)
-  reaper.ShowConsoleMsg(tostring(arg) .. "\n")
+function unselectMasterTrack()
+
+	local masterTrack = reaper.GetMasterTrack(activeProjectIndex)
+	reaper.SetTrackSelected(masterTrack, false)
 end
 
+function unselectAllTracks()
 
-function envelopePointDoesNotExistAtStart(trackEnvelopeObject)
+	local commandId = 40297
+  reaper.Main_OnCommand(commandId, 0)
 
-	local envelopePointIndexAtStart = reaper.GetEnvelopePointByTime(trackEnvelope, 0)
-	local returnValue, position, value, shape, selected, bezier = reaper.BR_EnvGetPoint(trackEnvelopeObject, envelopePointIndexAtStart)
-
-	return value == 1.0
+  unselectMasterTrack()
 end
 
-function addPoints(trackEnvelope)
+function restoreTrackSelections(selectedTrackIndices)
 
-	local trackEnvelopeObject = reaper.BR_EnvAlloc(trackEnvelope, false)
+	for i = 1, #selectedTrackIndices do
 
-	local active, visible, armed, inLane, laneHeight, defaultShape, minValue, maxValue, centerValue, type, faderScaling = reaper.BR_EnvGetProperties(trackEnvelopeObject, true, true, true, true, 0, 0, 0, 0, 0, 0, true)
-
-	if visible == false then
-		local commandId = 40406
-	  reaper.Main_OnCommand(commandId, 0)
-	end
-
-	if faderScaling == true then
-		minValue = reaper.ScaleToEnvelopeMode(1, minValue)
-		maxValue = reaper.ScaleToEnvelopeMode(1, maxValue)
-		centerValue = reaper.ScaleToEnvelopeMode(1, centerValue)
-	end
-
-	env_points_count = reaper.CountEnvelopePoints(trackEnvelope)
-
-	if env_points_count > 0 then
-		for k = 0, env_points_count+1 do
-			reaper.SetEnvelopePoint(trackEnvelope, k, timeInOptional, valueInOptional, shapeInOptional, tensionInOptional, false, true)
+		if selectedTrackIndices[i] == 0 then
+			local track = reaper.GetMasterTrack(activeProjectIndex)
+			reaper.SetTrackSelected(track, true)
+		else
+			local track = reaper.GetTrack(activeProjectIndex, selectedTrackIndices[i]-1)
+			reaper.SetTrackSelected(track, true)
 		end
 	end
-
-	firstStartValue, lastStartValue, firstEndValue, lastEndValue = getDeleteTimeLoopPoints(trackEnvelope, env_points_count, startTime, endTime)
-
-	local minValue = 0.0
-
-	if envelopePointDoesNotExistAtStart(trackEnvelopeObject) then
-		reaper.InsertEnvelopePoint(trackEnvelope, 0, minValue, 0, 0, true, true)
-	end
-	
-	
-	local currentBpm = reaper.TimeMap2_GetDividedBpmAtTime(0, 0)
-	local lengthOfSixteenthNoteInSeconds = 60000/currentBpm/4/1000
-
-	reaper.InsertEnvelopePoint(trackEnvelope, startTime-lengthOfSixteenthNoteInSeconds, minValue, 4, 0, true, true)
-	reaper.InsertEnvelopePoint(trackEnvelope, startTime, centerValue, 0, 0, true, true)
-
-	reaper.InsertEnvelopePoint(trackEnvelope, endTime, centerValue, 3, 0, true, true)
-	reaper.InsertEnvelopePoint(trackEnvelope, endTime+lengthOfSixteenthNoteInSeconds, minValue, 0, 0, true, true)
-
-
-	reaper.BR_EnvFree(trackEnvelopeObject, 0)
-	reaper.Envelope_SortPoints(trackEnvelope)
 end
 
-
-function getTimeSelectionEdges()
-	return reaper.GetSet_LoopTimeRange2(0, false, false, 0, 0, false)
-end
-
-function timeSelectionExists()
-	local startTime, endTime = getTimeSelectionEdges()
-	return startTime ~= endTime
-end
+--
 
 function volumeEnvelopeIsNotVisible(trackEnvelope)
 
-	local trackEnvelopeObject = reaper.BR_EnvAlloc(trackEnvelope, false)
+	local takeEnvelopesUseProjectTime = true
+	local trackEnvelopeObject = reaper.BR_EnvAlloc(trackEnvelope, takeEnvelopesUseProjectTime)
+
 	local active, visible, armed, inLane, laneHeight, defaultShape, minValue, maxValue, centerValue, type, faderScaling = reaper.BR_EnvGetProperties(trackEnvelopeObject, true, true, true, true, 0, 0, 0, 0, 0, 0, true)
-	reaper.BR_EnvFree(trackEnvelopeObject, 0)
+	
+	local commitChanges = false
+	reaper.BR_EnvFree(trackEnvelopeObject, commitChanges)
+	
 	return visible == false
 end
 
-function toggleVolumeEnvelopeVisibility()
+function toggleTrackVolumeEnvelopeVisibility()
 
 	local commandId = 40406
   reaper.Main_OnCommand(commandId, 0)
 end
 
-function main() -- local (i, j, item, take, track)
+function showVolumeEnvelopes()
 
-	startUndoBlock()
+	local numberOfSelectedItems = reaper.CountSelectedMediaItems(activeProjectIndex)
 
-	startTime, endTime = getTimeSelectionEdges()
+	for i = 0, numberOfSelectedItems - 1 do
 
-	if timeSelectionExists() then
+		local selectedItem = reaper.GetSelectedMediaItem(activeProjectIndex, i)
+		local trackOfSelectedItem = reaper.GetMediaItem_Track(selectedItem)
+		local trackEnvelope = reaper.GetTrackEnvelopeByName(trackOfSelectedItem, "Volume")
 
-		-- ROUND LOOP TIME SELECTION EDGES
-		--startTime = math.floor(startTime * 100000000+0.5)/100000000
-		--endTime = math.floor(endTime * 100000000+0.5)/100000000
-
-		numberOfSelectedTracks = reaper.CountSelectedTracks(0)
-
-		for i = 0, numberOfSelectedTracks-1  do
-
-			selectedTrack = reaper.GetSelectedTrack(0, i)
-			trackEnvelope = reaper.GetTrackEnvelopeByName(selectedTrack, "Volume")
-
-			if volumeEnvelopeIsNotVisible(trackEnvelope) then
-				toggleVolumeEnvelopeVisibility()
-			else
-				addPoints(trackEnvelope)
-			end
+		if volumeEnvelopeIsNotVisible(trackEnvelope) then
+			reaper.SetTrackSelected(trackOfSelectedItem, true)
 		end
-
-		endUndoBlock("isolateItemVolume")
 	end
+
+	toggleTrackVolumeEnvelopeVisibility()
+	unselectAllTracks()
+end
+
+--
+
+function getStartPosition(item)
+	local itemPosition = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+	return itemPosition
+end
+
+function getEndPosition(item)
+	local itemPosition = reaper.GetMediaItemInfo_Value(item, "D_POSITION")
+	local itemLength = reaper.GetMediaItemInfo_Value(item, "D_LENGTH")
+	return itemPosition + itemLength
+end
+
+--
+
+function startingEnvelopePointIsAtCenterValue(trackEnvelope)
+
+	local timePosition = 0
+	local envelopePointIndexAtStart = reaper.GetEnvelopePointByTime(trackEnvelope, timePosition)
+	local returnValue, time, value, shape, tension, selected = reaper.GetEnvelopePoint(trackEnvelope, envelopePointIndexAtStart)
+	return value == 1.0
 
 end
 
+function linearShape() 				return 0 end
+function squareShape() 				return 1 end
+function slowStartEndShape() 	return 2 end
+function fastStartShape() 		return 3 end
+function fastEndShape() 			return 4 end
+function bezierShape() 				return 5 end
+
+--
+
+function addEnvelopePoints(trackEnvelope, startPosition, endPosition, noteLength)
+
+	local selected = false
+	local noSort = true
+	local tension = 0.0
+
+	local minValue = 0.0
+	local centerValue = 1.0
 
 
-reaper.PreventUIRefresh(1)
-main()
-reaper.PreventUIRefresh(-1) 
-reaper.UpdateArrange()
-redrawEnvelope()
-reaper.defer(emptyFunctionToPreventAutomaticCreationOfUndoPoint)
+	if startPosition == 0.0 then
+
+		if not startingEnvelopePointIsAtCenterValue(trackEnvelope) then
+			reaper.InsertEnvelopePoint(trackEnvelope, 0.0, centerValue, linearShape(), tension, selected, noSort)
+		end
+	
+	else
+
+		if startPosition-noteLength < 0.0 then
+			reaper.InsertEnvelopePoint(trackEnvelope, 0.0, minValue, fastEndShape(), tension, selected, noSort)
+		else
+
+			if startingEnvelopePointIsAtCenterValue(trackEnvelope) then
+				reaper.InsertEnvelopePoint(trackEnvelope, 0.0, minValue, linearShape(), tension, selected, noSort)
+			end
+
+			reaper.InsertEnvelopePoint(trackEnvelope, startPosition-noteLength, minValue, fastEndShape(), tension, selected, noSort)
+		end
+
+		reaper.InsertEnvelopePoint(trackEnvelope, startPosition, centerValue, linearShape(), tension, selected, noSort)
+	end
+
+	reaper.InsertEnvelopePoint(trackEnvelope, endPosition-noteLength/4, centerValue, fastStartShape(), tension, selected, noSort)
+	reaper.InsertEnvelopePoint(trackEnvelope, endPosition, minValue, linearShape(), tension, selected, noSort)
+
+	reaper.Envelope_SortPoints(trackEnvelope)
+end
+
+
+function isolateItems()
+
+	local numberOfSelectedItems = reaper.CountSelectedMediaItems(activeProjectIndex)
+
+	for i = 0, numberOfSelectedItems - 1 do
+
+		local selectedItem = reaper.GetSelectedMediaItem(activeProjectIndex, i)
+		local trackOfSelectedItem = reaper.GetMediaItem_Track(selectedItem)
+
+		local trackEnvelope = reaper.GetTrackEnvelopeByName(trackOfSelectedItem, "Volume")
+		local startPosition = getStartPosition(selectedItem)
+		local endPosition = getEndPosition(selectedItem)
+		local noteLength = lengthOfHundredTwentyEighthNote()
+		addEnvelopePoints(trackEnvelope, startPosition, endPosition, noteLength)
+	end
+end
+
+-----
+
+local numberOfSelectedItems = reaper.CountSelectedMediaItems(activeProjectIndex)
+
+if numberOfSelectedItems == 0 then
+	reaper.defer(emptyFunctionToPreventAutomaticCreationOfUndoPoint)
+	return
+end
+
+startUndoBlock()
+
+	local selectedTrackIndices = getIndicesOfSelectedTracks()
+	unselectAllTracks()
+	showVolumeEnvelopes()
+	isolateItems()
+	restoreTrackSelections(selectedTrackIndices)
+	reaper.UpdateArrange()
+
+endUndoBlock()
